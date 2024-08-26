e8531e834396925da820c00f006c1a58c9996595
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-26 16:52:45 +0530

    Create LocalDateTimeExp.java

---
+++ LocalDateTimeExp.java
@@ -0,0 +1,4 @@
+public class LocalDateTimeExp{
+// Write a method getCurrentDateTime() that returns the current date and time as a LocalDateTime object. Also, print the date-time in the format yyyy-MM-dd HH:mm:ss.
+  //  Write a method isDateTimeBefore(LocalDateTime dateTime1, LocalDateTime dateTime2) that takes two LocalDateTime objects and returns true if dateTime1 is before dateTime2, otherwise returns false.
+}
