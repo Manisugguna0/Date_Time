bc693e690841c5c758d753d4ab0bf71074f46966
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-26 16:51:24 +0530

    Create LocalTimeExp.java

---
+++ LocalTimeExp.java
@@ -0,0 +1,4 @@
+public class LocalTimeExp{
+// : Write a method addHoursToTime(LocalTime time, int hours) that takes a LocalTime object and an integer representing hours, and returns a new LocalTime with the specified number of hours added.
+// Write a method compareTimes(LocalTime time1, LocalTime time2) that compares two LocalTime objects and returns:
+}
