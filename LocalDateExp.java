70ef18a55f278fbec3f9f2301ee4e9a6d0e7dc6f
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-26 16:49:30 +0530

    Update LocalDateExp.java

--- LocalDateExp.java
+++ LocalDateExp.java
@@ -1,3 +1,4 @@
 public class LocalDateExp{
 // Create a method addDaysToCurrentDate(int days) that adds a specified number of days to the current date and returns the new date.
+  // Create a method getFirstDayOfMonth(LocalDate date) that returns the first day of the month for a given date.
 }
